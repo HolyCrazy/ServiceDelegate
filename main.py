1f43179273d207575f6b2ee674ce31e25d6d2030
liduo <liduo.liduo@bytedance.com>
2023-12-11 19:59:57 +0800

    feat: add emoji service

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import json
 from io import BytesIO
-
+import requests
 from fastapi import FastAPI
 from pydantic import BaseModel
-import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
 app = FastAPI()
 
@@ -11,18 +14,32 @@ class ImageInfo(BaseModel):
     url: str
 
 
+class EmojiServiceParam(BaseModel):
+    firstEmoji: str
+    secondEmoji: str
+
+
 @app.get("/")
 async def get_main():
     return {"message": "service is running"}
 
 
-@app.post("/ocr/")
-async def print_repo_url(info: ImageInfo):
-    url = info.url
-    response = requests.get(url)
-    image_in_memory = BytesIO(response.content)
-    response = requests.post('https://api.oioweb.cn/api/ocr/recognition', files={'file': image_in_memory})
-    return {"response": response}
+@app.post("/emoji_kitchen/")
+async def emoji_service(param: EmojiServiceParam):
+    response = load_page("https://emoji.supply/kitchen/?" + param.firstEmoji + "+" + param.secondEmoji)
+    soup = BeautifulSoup(response, 'html.parser')
+    image_src = soup.find('img', id='pc').get('src')
+    return {"image_url": image_src}
+
+
+def load_page(url):
+    options = Options()
+    options.add_argument("--headless")
+    driver = webdriver.Firefox(options=options)
+    driver.get(url)
+    content = driver.page_source
+    driver.quit()
+    return content
 
 
 if __name__ == "__main__":
